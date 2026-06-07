3e6dabd4e9bcfc9e0ea4da0660193ceadbe75013
oleti15 <babyoleti333@gmail.com>
2026-06-07 20:06:52 +0530

    Added overloading.py

---
+++ overloading.py
@@ -0,0 +1,10 @@
+# Overloading is a concept of Exibiting different types 
+# behaviour in output based on input.
+
+# class Addition:
+#     def add(self,*args):
+#         print(sum(args))
+# ob=Addition()
+# ob.add(1,2,3)
+# ob.add(7,6,5,4)
+
